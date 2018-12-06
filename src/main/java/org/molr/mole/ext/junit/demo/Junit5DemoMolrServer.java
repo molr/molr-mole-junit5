ed4623310695fccfed4392b794fefec069b2ed59
Andrea Calia <andrea.calia.46@gmail.com>
2018-12-06 10:13:09 +0100

    LocalSuperMole is now in mole-core so embedding is easier. Simplified mole-server configuration

--- src/main/java/org/molr/mole/ext/junit/demo/Junit5DemoMolrServer.java
+++ src/main/java/org/molr/mole/ext/junit/demo/Junit5DemoMolrServer.java
@@ -1,13 +1,13 @@
 package org.molr.mole.ext.junit.demo;
 
-import org.molr.mole.server.conf.LocalMolrConfiguration;
-import org.molr.mole.server.conf.MolrRestServiceConfiguration;
+import org.molr.mole.core.conf.LocalSuperMoleConfiguration;
+import org.molr.mole.server.conf.SingleMoleRestServiceConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import({LocalMolrConfiguration.class, MolrRestServiceConfiguration.class})
+@Import({LocalSuperMoleConfiguration.class, SingleMoleRestServiceConfiguration.class})
 public class Junit5DemoMolrServer {
 
     public static void main(String... args) {
